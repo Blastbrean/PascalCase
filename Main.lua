fdd3c6cb02f21f6a12cdd34999c9a234f0ec26b0
Blastbrean <73199280+retarded-person@users.noreply.github.com>
2023-06-14 19:36:39 -0700

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,13 +1,15 @@
-local PlaceIdToScriptTable = {
-	["142823291"] = "https://raw.githubusercontent.com/retarded-person/PascalCase-MM2/main/Compiler/Output.lua",
-	["10138901829"] = "https://raw.githubusercontent.com/retarded-person/PascalCase-DDev/main/Compiler/Output.lua",
-}
+pcall(function()
+	-- Script to PlaceId table
+	local PlaceIdToScriptTable = {
+		["10138901829"] = "https://raw.githubusercontent.com/retarded-person/PascalCase-DDev/main/Compiler/Output.lua",
+	}
 
--- Attempt to get a URL to load from with our PlaceId...
-local ScriptString = PlaceIdToScriptTable[tostring(game.PlaceId)]
-if not ScriptString then
-	return warn("PascalCase does not have a script for your current game!")
-end
+	-- Attempt to get a URL to load from with our PlaceId...
+	local ScriptString = PlaceIdToScriptTable[tostring(game.PlaceId)]
+	if not ScriptString then
+		return warn("PascalCase does not have a script for your current game!")
+	end
 
--- Load script...
-loadstring(game:HttpGet(ScriptString))()
+	-- Load script...
+	loadstring(game:HttpGet(ScriptString))()
+end, print)
