217dcf516b94a1876d6a65a47629bf6a8b222a10
Blastbrean <73199280+retarded-person@users.noreply.github.com>
2023-06-15 01:32:53 +0000

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,15 +1,13 @@
-local HttpGet = game["HttpGet"]
-local PlaceId = game["PlaceId"]
 local PlaceIdToScriptTable = {
 	["142823291"] = "https://raw.githubusercontent.com/retarded-person/PascalCase-MM2/main/Compiler/Output.lua",
 	["10138901829"] = "https://raw.githubusercontent.com/retarded-person/PascalCase-DDev/main/Compiler/Output.lua",
 }
 
 -- Attempt to get a URL to load from with our PlaceId...
-local ScriptString = PlaceIdToScriptTable[PlaceId]
+local ScriptString = PlaceIdToScriptTable[game.PlaceId]
 if not ScriptString then
 	return warn("PascalCase does not have a script for your current game!")
 end
 
 -- Load script...
-loadstring(HttpGet(ScriptString))()
+loadstring(game:HttpGet(ScriptString))()
