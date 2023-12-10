1e547d73bb0648c94fcaf59b2ac8bc493aa46b57
Blastbrean <73199280+Blastbrean@users.noreply.github.com>
2023-12-10 18:36:21 +0000

    Update PascalCase-ddev URL

--- Main.lua
+++ Main.lua
@@ -1,7 +1,7 @@
 pcall(function()
 	-- Script to PlaceId table
 	local PlaceIdToScriptTable = {
-		[10371908957] = "https://raw.githubusercontent.com/retarded-person/PascalCase-ddev/main/Compiler/Output.lua",
+		[10371908957] = "https://raw.githubusercontent.com/Blastbrean/PascalCase-ddev/main/Compiler/Output.lua",
 	}
 
 	-- Deepwoken PlaceIds...
